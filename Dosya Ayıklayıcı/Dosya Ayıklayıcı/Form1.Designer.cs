d2fa4d0e9f5f7eeda691ed7234c5898a22536377
Alihan Dursun <dursunalihan@gmail.com>
2023-10-07 15:31:04 +0300

    Dosya Ayıklayıcı
    Bu dosya ayıklama programı ile belirli bir klasör veya disk içindeki dosyaları ayıklayabilirisiniz.

---
+++ Dosya Ayıklayıcı/Dosya Ayıklayıcı/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Dosya_Ayıklayıcı
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(81, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(563, 87);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kaynak Klasör/Disk";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(16, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(519, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Seç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(81, 105);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(563, 87);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Hedef Klasör/Disk";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(16, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(519, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Seç";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Hepsi",
+            "İçeriğe göre"});
+            this.comboBox1.Location = new System.Drawing.Point(3, 40);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ayıklama tipi : ";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 67);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Ayıkla";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(144, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(313, 66);
+            this.panel1.TabIndex = 6;
+            this.panel1.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(7, 29);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(286, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "İçerik : ";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Controls.Add(this.comboBox1);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Location = new System.Drawing.Point(81, 209);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(563, 115);
+            this.panel2.TabIndex = 7;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(81, 330);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(563, 147);
+            this.listBox1.TabIndex = 8;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 486);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Dosya Ayıklayıcı";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Dosya Ayıklayıcı/Dosya Ayıklayıcı/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Dosya_Ayıklayıcı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            textBox1.Text = folderBrowserDialog1.SelectedPath.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            textBox2.Text = folderBrowserDialog1.SelectedPath.ToString();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 1)
+            {
+                panel1.Visible = true;
+            }
+            else
+            {
+                panel1.Visible = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (String kaynak in Directory.GetDirectories(textBox1.Text))
+            {
+                listBox1.Items.Add(kaynak);
+                foreach (string klasor1 in Directory.GetDirectories(kaynak))
+                {
+                    listBox1.Items.Add(klasor1);
+                    foreach (string klasor2 in Directory.GetDirectories(klasor1))
+                    {
+                        listBox1.Items.Add(klasor2);
+                        foreach (string klasor3 in Directory.GetDirectories(klasor2))
+                        {
+                            listBox1.Items.Add(klasor3);
+                            foreach (string klasor4 in Directory.GetDirectories(klasor3))
+                            {
+                                listBox1.Items.Add(klasor4);
+                                foreach (string klasor5 in Directory.GetDirectories(klasor4))
+                                {
+                                    listBox1.Items.Add(klasor5);
+                                    foreach (string klasor6 in Directory.GetDirectories(klasor5))
+                                    {
+                                        listBox1.Items.Add(klasor6);
+                                        foreach (string klasor7 in Directory.GetDirectories(klasor6))
+                                        {
+                                            listBox1.Items.Add(klasor7);
+                                            foreach (string klasor8 in Directory.GetDirectories(klasor7))
+                                            {
+                                                listBox1.Items.Add(klasor8);
+                                                foreach (string klasor9 in Directory.GetDirectories(klasor8))
+                                                {
+                                                    listBox1.Items.Add(klasor9);
+                                                    foreach (string klasor10 in Directory.GetDirectories(klasor9))
+                                                    {
+                                                        listBox1.Items.Add(klasor10);
+                                                        foreach (string klasor11 in Directory.GetDirectories(klasor10))
+                                                        {
+                                                            listBox1.Items.Add(klasor11);
+                                                            foreach (string klasor12 in Directory.GetDirectories(klasor11))
+                                                            {
+                                                                listBox1.Items.Add(klasor12);
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            foreach (string dosya in Directory.GetFiles(textBox1.Text))
+            {
+                openFileDialog1.FileName = dosya;
+                string dsyad = openFileDialog1.SafeFileName;
+
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    if (File.Exists(textBox2.Text + "\\" + dsyad))
+                    {
+                        dsyad = "(-2-)" + dsyad;
+                    }
+                    File.Move(dosya, textBox2.Text + "\\" + dsyad);
+                }
+                else if (dsyad.Contains(textBox3.Text))
+                {
+                    if (File.Exists(textBox2.Text + "\\" + dsyad))
+                    {
+                        dsyad = "(-2-)" + dsyad;
+                    }
+                    File.Move(dosya, textBox2.Text + "\\" + dsyad);
+                }
+            }
+
+            foreach (string klasoryol in listBox1.Items)
+            {
+                foreach (string dosya in Directory.GetFiles(klasoryol))
+                {
+                    openFileDialog1.FileName = dosya;
+                    string dsyad = openFileDialog1.SafeFileName;
+
+                    if (comboBox1.SelectedIndex == 0)
+                    {
+                        if (File.Exists(textBox2.Text + "\\" + dsyad))
+                        {
+                            dsyad = "(-2-)" + dsyad;
+                        }
+                        File.Move(dosya, textBox2.Text + "\\" + dsyad);
+                    }
+                    else if (dsyad.Contains(textBox3.Text))
+                    {
+                        if (File.Exists(textBox2.Text + "\\" + dsyad))
+                        {
+                            dsyad = "(-2-)" + dsyad;
+                        }
+                        File.Move(dosya, textBox2.Text + "\\" + dsyad);
+                    }
+                }
+              
+            }
+
+            textBox3.Text = null;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
+    }
+}
